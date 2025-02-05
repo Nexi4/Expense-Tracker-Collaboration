5a64354164d48e60feb594d5276782434be2fb01
Nexi4 <trevorau22@gmail.com>
2025-02-05 11:31:48 -0800

    Update #4 Expense_Tracker_Collaboration.py

--- Expense_Tracker_Collaboration.py
+++ Expense_Tracker_Collaboration.py
@@ -1,7 +1,23 @@
 expense_listings = {}
+from datetime import datetime
 on = True
+def main_menu():
+    global on
+    while on:
+        choice = input("Choose one of the options: \n1) Add Expense\n2) View Expense\n3) Total Expense\n4) Delete Expense\n5) Exit\n")
+        if choice == '1':
+            add_expense()
+        elif choice == '2':
+            view_expense()
+        elif choice == '3':
+            total_expense()
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            on = False
+
 def add_expense():
-        categories = ["Food", "Clothing", "Utility", "Entertainment", "Transport", "Healthcare", "Insurance", "Housing", "Internet", "Other"]
+        categories = ["Food", "Clothing", "Utility", "Entertainment", "Transport", "Healthcare", "Insurance", "Housing", "Internet", "Other\n"]
         while True:
             expense_category = input(f"Add what kind of expense?\n Choose from {categories} ").capitalize()
             if expense_category not in categories:
@@ -21,7 +37,14 @@ def add_expense():
             print("Not a number, try again.")
             return
 
-        expense_time = input("When was the expense(YYYY-MM-DD)?: ")
+        while True:
+            date_attempt = input("When was the expense(YYYY-MM-DD)?: ")
+            try:
+                expense_time = datetime.strptime(date_attempt, "%Y-%m-%d").date()
+                break
+            except ValueError:
+                print("Enter time in given format.")
+
         
         if expense_category not in expense_listings:
             expense_listings[expense_category] = []
@@ -31,8 +54,7 @@ def add_expense():
                 'Time of expense': expense_time
             })
 
-        print(f"Expense added:\n {expense_category}: Spent ${expense_amount} on {expense_time}")
-add_expense()
+        print(f"Expense added:\n {expense_category}: Spent ${expense_amount} on {expense_time}\n")
 
 def view_expense():
     if not expense_listings:
@@ -40,27 +62,14 @@ def view_expense():
         return
     
     for expense_category, expenses in expense_listings.items():
+        print("---------Expenses----------")
         print(f"\nCategory: {expense_category}")
         for expense in expenses:
-            print(f" Amount: ${expense['Amount spent']}\n Time: {expense['Time of expense']}")
-view_expense()
-
+            
+            print(f" Amount: ${expense['Amount spent']}\n Time: {expense['Time of expense']}\n")
+            print("---------------------------")
+ 
 def total_expense():
     pass
 
-on = True
-while on:
-    def main_menu():
-        choice = input("Choose one of the options: \n1) Add Expense\n2) View Expense\n3) Total Expense\n4) Delete Expense\n5) Exit\n")
-        if choice == '1':
-            add_expense()
-        elif choice == '2':
-            view_expense()
-        elif choice == '3':
-            total_expense()
-        elif choice == '4':
-            pass
-        elif choice == '5':
-            on = False
-    main_menu()
-    
+main_menu()
